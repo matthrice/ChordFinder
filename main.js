13c85024e69e7645027f44993b76764e1881d906
matthrice <mattrice@umich.edu>
2017-05-23 19:23:39 -0500

    added inversions

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-var chords = [[4, 3], [3, 4], [4, 3, 3], [4, 3, 4], [3, 4, 3]];
-var chordNames = ["major", "Cminor", "dom7", "maj7", "min7"];
+var chords = [[4, 3], [3, 4], [4, 3, 3], [4, 3, 4], [3, 4, 3], [3, 3], [5, 2]];
+var chordNames = ["major", "minor", "dom7", "maj7", "min7", "dim", "sus"];
 
+
+function invert(spacings) {
+	var base = spacings[0];
+	var invert_size = (spacings[spacings.length - 1] - spacings[0]) < 12 ? 12 : 24;
+	var total = 0;
+	for (var i = 0; i < spacings.length; i++) {
+		total += spacings[i];
+	}
+	spacings.shift();
+	spacings.push(invert_size - total);
+	return spacings;
+}
 
 function findChord(notes) {
 	var spacings = [];
@@ -8,19 +20,23 @@ function findChord(notes) {
 	for (var i = 1; i < notes.length; i++) {
 		spacings.push(notes[i] - notes[i - 1]);
 	}
-	for (var i = 0; i < chords.length; i++) {
-		equal = true;
-		if (spacings.length == chords[i].length) {
-			for (var k = 0; k < spacings.length; k++) {
-				if (spacings[k] != chords[i][k]) {
-					equal = false;
-					break;
+	for (var j = 0; j < notes.length; j++) {
+		for (var i = 0; i < chords.length; i++) {
+			equal = true;
+			if (spacings.length == chords[i].length) {
+				for (var k = 0; k < spacings.length; k++) {
+					if (spacings[k] != chords[i][k]) {
+						equal = false;
+						break;
+					}
+				}
+				if (equal) {
+					return chordNames[i];
 				}
 			}
-			if (equal) {
-				return chordNames[i];
-			}
 		}
+		spacings = invert(spacings);
+		alert(spacings);
 	}
 	return "Does not exist";
 }
