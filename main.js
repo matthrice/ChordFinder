43ada530060a63dffe140ee3387a210b229a8ad7
matthrice <mattrice@umich.edu>
2017-05-24 16:46:15 -0500

    Added octave/redundancy removal

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
 var chords = [[4, 3], [3, 4], [4, 3, 3], [4, 3, 4], [3, 4, 3], [3, 3], [5, 2]];
 var shapes = ["major", "minor", "7", "maj7", "min7", "dim", "sus"];
+var num_notes = 12;
 
-
+/* 
+ * Removes octave to avoid redundancy and simplify identification
+ * Allows the user to use the whole keyboard but lets the program
+ * only deal with 12 keys instead of 24
+ */
+function removeOctaves(notes) {
+	var new_note = 0;
+	var exists = false;
+	for (var i = 0; i < notes.length; i++) {
+		if (notes[i] > 11) {
+			new_note = notes[i] - 12;
+			for (var k = 0; k < notes.length; k++) {
+				if (new_note == notes[k]) {
+					exists = true;
+					break;
+				}
+			}
+			if (!exists) {
+				notes[i] = new_note;
+			}
+			else {
+				notes.splice(i, 1);
+			}
+		}
+		exists = false;
+	}
+	// notes sorted to account for out of order formations
+	return notes.sort();
+}
+/*
+ * Inverts the chord based on its current inversion
+ * Cmaj I -> Cmaj II etc.
+ */
 function invert(spacings) {
 	var base = spacings[0];
-	var invert_size = (spacings[spacings.length - 1] - spacings[0]) < 12 ? 12 : 24;
 	var total = 0;
 	for (var i = 0; i < spacings.length; i++) {
 		total += spacings[i];
 	}
 	spacings.shift();
-	spacings.push(invert_size - total);
+	spacings.push(num_notes - total);
 	return spacings;
 }
 
+/*
+ * Finds chord by first reducing notes to an array of spacings between notes
+ * Linear searches the spacings for an existing formation
+ * If not found, inverts chord and tries again
+ * Returns both number of inversions (to find root) and the formation 
+ */
 function findChord(notes) {
+	notes = removeOctaves(notes);
 	var spacings = [];
 	var equal = true;
 	var fullChord = {num_inversions: 0, shape: "Does not exist"}
@@ -44,7 +83,5 @@ function findChord(notes) {
 	return fullChord;
 }
 
-var exChord = [1, 4, 8];
-var name = findChord(exChord);
 
 
