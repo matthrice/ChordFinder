def9d38d0e568b732687ab7c0df71e9d4dd53347
matthrice <mattrice@umich.edu>
2017-06-03 13:44:49 -0500

    completed wikipedia links

--- chords.js
+++ chords.js
@@ -1,7 +1,7 @@
 var wiki_url = ["https://en.wikipedia.org/wiki/", "_(musical_note)"];
 
 var chords1 = {
-	"8": [], 
+	"8": [],  
 	"mi2": [1],
 	"maj2": [2],
 	"mi3": [3],
@@ -15,14 +15,38 @@ var chords1 = {
 	"maj7": [11]
 }
 
+var links1 = [
+	"https://en.wikipedia.org/wiki/Octave",
+	"https://en.wikipedia.org/wiki/Semitone#Minor_second",
+	"https://en.wikipedia.org/wiki/Major_second",
+	"https://en.wikipedia.org/wiki/Minor_third",
+	"https://en.wikipedia.org/wiki/Major_third",
+	"https://en.wikipedia.org/wiki/Perfect_fourth",
+	"https://en.wikipedia.org/wiki/Tritone",
+	"https://en.wikipedia.org/wiki/Perfect_fifth",
+	"https://en.wikipedia.org/wiki/Minor_sixth",
+	"https://en.wikipedia.org/wiki/Major_sixth",
+	"https://en.wikipedia.org/wiki/Minor_seventh",
+	"https://en.wikipedia.org/wiki/Major_seventh"
+]
+
 var chords2 = {
 	"major": [4, 3],
 	"minor": [3, 4],
 	"dim": [3, 3],
 	"sus4": [5, 2],
-	"maj7": [4, 6],
+	"maj7": [4, 7],
 	"aug": [4, 4]
 }
+
+var links2 = [
+	"https://en.wikipedia.org/wiki/Major_chord",
+	"https://en.wikipedia.org/wiki/Minor_chord",
+	"https://en.wikipedia.org/wiki/Diminished_triad",
+	"https://en.wikipedia.org/wiki/Suspended_chord",
+	"https://en.wikipedia.org/wiki/Major_seventh_chord",
+	"https://en.wikipedia.org/wiki/Augmented_triad"
+]
 
 var chords3 = {
 	"7": [4, 3, 3],
@@ -38,3 +62,17 @@ var chords3 = {
 	"mi aug7": [4, 4, 2],
 	"add 9": [3, 4, 7]
 }
+
+var links3 = [
+	"https://en.wikipedia.org/wiki/Dominant_seventh_chord",
+	"https://en.wikipedia.org/wiki/Major_seventh_chord",
+	"https://en.wikipedia.org/wiki/Minor_seventh_chord",
+	"https://en.wikipedia.org/wiki/Sixth_chord",
+	"https://en.wikipedia.org/wiki/Diminished_seventh_chord",
+	"https://en.wikipedia.org/wiki/Dominant_seventh_flat_five_chord",
+	"https://en.wikipedia.org/wiki/Half-diminished_seventh_chord",
+	"https://en.wikipedia.org/wiki/Minor_major_seventh_chord",
+	"https://en.wikipedia.org/wiki/Augmented_major_seventh_chord",
+	"https://en.wikipedia.org/wiki/Augmented_seventh_chord",
+	"https://en.wikipedia.org/wiki/Ninth_chord"
+]
